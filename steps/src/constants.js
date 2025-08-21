472bc2a1908aaa797a12268b6065e3429588bd5d
rakadu1 <rakadu1@bajajauto.co.in>
2025-08-21 16:04:56 +0530

    refactor: Extract constants into constants.js

---
+++ steps/src/constants.js
@@ -0,0 +1,7 @@
+export const MAX_TRAJECTORY_LENGTH = 50;
+export const VIDEO_FPS = 30;
+
+export const RADAR_X_MIN = -20;
+export const RADAR_X_MAX = 20;
+export const RADAR_Y_MIN = 0;
+export const  RADAR_Y_MAX = 60;
