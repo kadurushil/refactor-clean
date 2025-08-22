54b0a4f0f42b412ff764f7aac03d139f1c069d20
rakadu1 <rakadu1@bajajauto.co.in>
2025-08-22 10:11:15 +0530

    State.js creation and global state moved out

---
+++ steps/src/state.js
@@ -0,0 +1,19 @@
+export const appState = {
+
+
+     vizData : null,
+     canData : [],
+     rawCanLogText : null,
+     videoStartDate : null,
+     radarStartTimeMs : 0,
+     isPlaying : false,
+     currentFrame : 0,
+     globalMinSnr : 0, globalMaxSnr : 1,
+     p5_instance : null, speedGraphInstance : null,
+     jsonFilename : '', videoFilename : '', canLogFilename : '',
+     isCloseUpMode : false,
+     masterClockStart : 0,
+     mediaTimeStart : 0,
+     lastSyncTime : 0,
+
+};
